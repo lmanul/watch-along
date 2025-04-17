b70bf97861fae6e8613e528a3b84cd6ee32d0ff3
Manu Cornet <m@ma.nu>
2025-04-17 16:11:04 +0000

    Getting to a simple video player state

--- static/player.js
+++ static/player.js
@@ -3,27 +3,33 @@ const concatenateUint8Arrays = (array1, array2) => {
   concatenatedArray.set(array1, 0);
   concatenatedArray.set(array2, array1.length);
   return concatenatedArray;
-}
+};
 
 const main = async () => {
-
-  const video = document.getElementById('video');
-  const videoSrc = '/video/playlist.m3u8';
+  const video = document.getElementById("video");
+  const videoSrc = "/video/video.m3u8";
   if (Hls.isSupported()) {
-    console.log('HLS supported');
+    console.log("HLS supported");
     const hls = new Hls();
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      console.log('Manifest parsed');
-       // video.play();
+
+    Object.keys(Hls.Events).forEach((eventName) => {
+      hls.on(Hls.Events[eventName], function (event, data) {
+        console.log(`Event: ${event}`, data);
+
+        if (event === Hls.Events.BUFFER_APPENDED) {
+          // Loop over buffered ranges
+          for (let i = 0; i < video.buffered.length; i++) {
+            const start = video.buffered.start(i);
+            const end = video.buffered.end(i);
+            console.log(`Buffered range: ${start} - ${end}`);
+          }
+        }
+      });
     });
-    console.log(Hls.Events);
-  } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-    video.src = videoSrc;
-    // video.addEventListener('loadedmetadata', function () {
-    //   video.play();
-    // });
+  } else {
+    alert('Video playback not supported here!');
   }
 };
 
